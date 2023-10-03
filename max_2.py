55a244bc1526e9a200521a921ff0095cc7a0b94a
Ellie McAllister <elliebugmc@gmail.com>
2023-10-02 21:32:12 -0700

    This is the solution

--- max_2.py
+++ max_2.py
@@ -1,11 +1,17 @@
 def max_of_three(numbers):
     result_max = numbers[0]
     index = 0
-    while result_max < numbers[index]:
-        result_max = numbers[index]
+    if len(numbers) > 0:
+        while index < len(numbers):
+            if numbers[index] > result_max:
+               result_max = numbers[index]
         index += 1
+    else: 
+        return 0
     return result_max
-print(max_of_three([1,2,3]))
+def main():
+    print(max_of_three([ ]))
+main()
 
-
+#This is in the midterm
 
